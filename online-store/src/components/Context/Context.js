eafd3f4deeee055ccba5fdea2d086fc83eb0a502
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-27 07:34:53 +0200

    context modifications

--- online-store/src/components/Context/Context.js
+++ online-store/src/components/Context/Context.js
@@ -1,5 +1,5 @@
 import React, {useState, createContext, useEffect} from 'react'
-import {commerce} from '../lib/commerce'
+import {commerce} from '../../lib/commerce'
 
 export const Context = createContext()
 
@@ -7,6 +7,7 @@ function GlobalContext({children}) {
 
     const [product, setProduct] = useState([])
     const [Items, setItems] = useState([])
+    const [checkoutToken, setCheckoutToken] = useState(null)
 
     const fetchProducts = async()=>{
         const {data} = await commerce.products.list()
@@ -21,11 +22,24 @@ function GlobalContext({children}) {
         commerce.cart.retrieve().then((cart)=>setItems(cart))
     },[])
 
+    useEffect(()=>{
+        const generateToken = async ()=>{
+            try {
+                await commerce.checkout.generateToken(Items.id, {type: 'cart'})
+                        .then((checkout)=>setCheckoutToken(checkout))
+            } catch (error) {
+                
+            }
+        }
+        generateToken()
+    },[Items])
+
     const value = {
         setItems,
         Items, 
         commerce,
         product,
+        checkoutToken,
     }
 
     return (
