701563cfec03a58c3a15935869641c311f53b57a
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-28 20:20:19 +0200

    final commit context

--- online-store/src/components/Context/Context.js
+++ online-store/src/components/Context/Context.js
@@ -1,5 +1,6 @@
 import React, {useState, createContext, useEffect} from 'react'
 import {commerce} from '../../lib/commerce'
+import {Redirect} from 'react-router-dom'
 
 export const Context = createContext()
 
@@ -8,6 +9,8 @@ function GlobalContext({children}) {
     const [product, setProduct] = useState([])
     const [Items, setItems] = useState([])
     const [checkoutToken, setCheckoutToken] = useState(null)
+    const [order, setOrder] = useState({})
+    const [error, setError] = useState('')
 
     const fetchProducts = async()=>{
         const {data} = await commerce.products.list()
@@ -28,11 +31,27 @@ function GlobalContext({children}) {
                 await commerce.checkout.generateToken(Items.id, {type: 'cart'})
                         .then((checkout)=>setCheckoutToken(checkout))
             } catch (error) {
-                
+                <Redirect to="/cart" />
             }
         }
         generateToken()
     },[Items])
+
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh()
+        setItems(newCart)
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenID, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenID, newOrder)
+
+            setOrder(incomingOrder)
+            refreshCart()
+        } catch (error) {
+            setError(error.data.error.message)
+        }
+    }
 
     const value = {
         setItems,
@@ -40,6 +59,9 @@ function GlobalContext({children}) {
         commerce,
         product,
         checkoutToken,
+        order,
+        handleCaptureCheckout,
+        error,
     }
 
     return (
