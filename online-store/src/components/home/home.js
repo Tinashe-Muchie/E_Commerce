19401d0fb5e5c4b93436949603aae2f3dc5ab749
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-03-28 20:20:31 +0200

    final commit home

--- online-store/src/components/home/home.js
+++ online-store/src/components/home/home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
 function Home() {
     return (
-        <div>
-            heloo
+        <div className="wrapper">
+            <div className="home-text my-1"> <span>Watches and Accessories that make you look the part, wherever you go!</span> 
+            <div className="mt-1">
+                <Link to="/watches">
+                    <Button variant="outline-primary">Shop now</Button>
+                </Link>
+            </div>    
+            </div>
+            <div className="sliding-background"></div>
         </div>
     )
 }
